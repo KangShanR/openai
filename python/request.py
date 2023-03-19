bb4d71feb8abefdfdc290271cdb215123b92a1c0
kang <kangshan9988@gmail.com>
2023-03-19 14:33:14 +0800

    sh generate the image by openai

--- python/request.py
+++ python/request.py
@@ -1,17 +1,22 @@
 import requests
 # find the prime numbers
 
-for i in range(2, 10):
-    for n in range(2, i):
-        if(i % n ==0):
-            print(i, 'equals', n, '*', i//n)
-            break
+def prime(p):
+    """Print the prime numbers from 2 2 the argument number"""
+    for i in range(2, p):
+        for n in range(2, i):
+            if(i % n ==0):
+                print(i, 'equals', n, '*', i//n)
+                break
+            else:
+                print(i, 'can\'t be divided by', n)
         else:
-            print(i, 'can\'t be divided by', n)
-    else:
-        print(i, 'is a prime number')
+            print(i, 'is a prime number')
+
+prime(100)
 
 # request
 
-response = request.get('43.136.134.205:8080/all')
+# response = request.get('43.136.134.205:8080/all')
+
 
