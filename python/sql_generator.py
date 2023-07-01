2fa7f57f8538a61cfa55dec8c177fa713e5ba7c9
kang <694206216@qq.com>
2023-07-02 00:39:39 +0800

    feat:  define a variable for sql template

--- python/sql_generator.py
+++ python/sql_generator.py
@@ -4,6 +4,6 @@ import requests
 def generateSql(start, end, template):
     """Print the prime numbers from start 2 end in the argument number"""
     for i in range(start, end):
-        print(template, i)
+        print(template.format(str(i).zfill(2)))
 
-generateSql(0, 100, "hello %d")
+generateSql(0, 100, "hello {}")
