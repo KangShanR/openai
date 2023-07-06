d591f16a5253ceb76fd0af08fe4cbe5f4630925a
k <694206216@qq.com>
2023-07-06 13:11:25 +0800

    feat: read excel data

--- python/excel.py
+++ python/excel.py
@@ -5,4 +5,12 @@ import os
 # import excel package
 import xlrd
 
-print("os.environ: " + str(os.environ))
+# print("os.environ: " + str(os.environ))
+import pandas as pd
+
+# Read Excel file
+df = pd.read_excel('~/Downloads/dict-adjust1.xlsx', engine='openpyxl')
+
+# Print the contents of the DataFrame
+for value in df['dict_code']:
+    print('"{}"'.format(value), end=', ')
