4fde61996feb18ef997603a7bbef2d39488e39b0
k <694206216@qq.com>
2023-07-14 18:12:16 +0800

    test

--- python/pandas.py
+++ python/pandas.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
-tb = pd.html.read_html("http://rate.bot.com.tw/xrt?Lang=zh-TW")
+# tb = pd.read_html("http://rate.bot.com.tw/xrt?Lang=zh-TW")
+tb = pd.read_html("https://baidu.com")
 
 print(tb[0])
