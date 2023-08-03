822943d3a1d4c8be43b98930b1271a1f7562b740
k <694206216@qq.com>
2023-08-03 15:14:22 +0800

    feat: excel comparasion

--- python/excel.py
+++ python/excel.py
@@ -1,20 +1,32 @@
 import xlrd
 import pandas as pd
 
-# Read Excel file
-df = pd.read_excel('~/Downloads/dict-adjust2.xlsx', engine='openpyxl')
+def compare_excel(excel1, excel2):
+    # Read Excel file
+    df = pd.read_excel(excel1, engine='openpyxl')
 
-template = '{}'
-columns = df['id']
+    data = {}
 
-length = len(columns)
-print(f'columns length: {length}')
+    for index, row in df.iterrows():
+        key = str(row['vender_id']) + '-' + str(row['receive_locale_id'])
+        value = str(row['bill_count']) +':' + str(row['item_count'])
+        data[key] = value
 
-# Print the contents of the DataFrame
-for index in range(length):
-    if index == 0:
-        print('\n\n\n\n\n')
-    elif index != length - 1:
-        print(template.format(columns[index]), end=', ')
-    else:
-        print(template.format(columns[index]))
+    store = pd.read_excel(excel2, engine='openpyxl')
+    store_data = {}
+    for index, row in store.iterrows():
+        key = str(row['vender_id']) + '-' + str(row['store_locale_id'])
+        value = str(row['order_count']) +':' + str(row['item_count'])
+        store_data[key] = value
+
+    for key, value in data.items():
+        store_value = store_data.pop(key)
+        if store_value != value:
+            print("Not equals " + str(key) +",bofc value:"+ str(value) +", store value:"+ str(store_value))
+        # else:
+        #     print("Equals " + key + ",value:" + value)
+
+    print('remain store data:' + str(store_data))
+
+
+compare_excel('~/Downloads/bofc.xlsx', '~/Downloads/store.xlsx')
