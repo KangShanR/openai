c722c229f0ee19776a41eec0e9b90b06c770d82e
kang <694206216@qq.com>
2025-02-16 12:42:03 +0800

    rename all the possible duplcate name

--- python/crontab.py
+++ python/crontab.py
@@ -1,17 +1,20 @@
 import datetime
-from mysql_connect import query_datas, query_data
+from mysql_connect import query_datas, engine
+import pandas as pd
 
 
 
 
 
-rows = query_datas("select * from crontab where data_state = \"VALID\";", None)
+rows = pd.read_sql("select * from crontab where data_state = \"VALID\";", engine)
 for row in rows:
     if(row[1] == "DAILY"):
-        logs = query_datas("""
-                           select * from cron_log where `name` = %s and data_state = \"VALID\" AND
-                            task_result = \"SUCCESS\" AND created_at >= %s
-                           """, (row[2], datetime.date.today().strftime("%Y-%m-%d %H:%M:%S")))
+        logs =pd.read_sql("""
+                            select * from cron_log where `name` = %s and data_state = \"VALID\" 
+                            AND task_result = \"SUCCESS\" AND created_at >= %s
+                        """,
+                        engine,
+                        params=(row[2],datetime.date.today().strftime("%Y-%m-%d %H:%M:%S")))
         for log in logs:
             print(log)
 
